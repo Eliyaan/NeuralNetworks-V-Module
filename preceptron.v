65abcb54cf7c65894f2416aab6ed88660114a05b
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-03-06 20:50:03 +0100

    working lib but needs some more features
    - relu / sigmoid / leaky relu
    - save the nn
    - re use the saved one

--- preceptron.v
+++ preceptron.v
@@ -2,29 +2,25 @@
 import rand as rd
 import math as m
 
-const(
-	nb_i = 2
-	nb_hl = 1
-	hd_neu = [3]
-	nb_o = 2
-	nb_epochs = 10000
-)
-
 
 struct NeuralNet{
 	//Consts
 	learning_rate f64
-	inputs [][]f64 
-	excpd_outputs [][]f64  // first : prob for 0 ; sec : prob for 1
 	nb_inputs int 
 	nb_hidden_layer int
 	nb_hidden_neurones []int 
 	nb_outputs int
 
+	shuffle_dataset bool
+	print_epoch int 
+
 mut:
 	weights_list [][][][]f64
 	layers_list [][][]f64  // bias, bias cost, nactiv, output(activ), cost
 	glob_output [][]f64
+	
+	inputs [][]f64 
+	excpd_outputs [][]f64  // first : prob for 0 ; sec : prob for 1
 
 }
 
@@ -124,7 +120,7 @@ fn (mut nn NeuralNet) backprop(index int){
 	//Reverif toute la backprop pour s'assurer qu'elle soit dans le bon sens avec les bons trucs car c pas ca x)
 	for i := nn.nb_hidden_layer; i>=0; i--{ 
 		hidd_lay := nn.layers_list[i]
-		if i == nn.nb_hidden_layer{ 
+		if i == nn.nb_hidden_layer{ //Correct
 			//Weights
 			for l, mut outputlist in nn.weights_list[i][1]{
 				for j, mut weight_cost in outputlist{
@@ -144,7 +140,7 @@ fn (mut nn NeuralNet) backprop(index int){
 			}else{
 				for l, mut hidden_cost in hidd_lay[4]{
 					for j in 0..nn.nb_outputs{//a changer pour I+1 pour le reste + faire un condi si au moins 1
-						hidden_cost += nn.weights_list[1][0][j][l]*nn.layers_list[1][2][j]*nn.layers_list[1][4][j]  // It's normal that we dont apply the dsig bcz already applied 
+						hidden_cost += nn.weights_list[1][0][j][l]*nn.layers_list[1][2][j]*(nn.layers_list[nn.nb_hidden_layer][3][j]-nn.excpd_outputs[index][j])
 					}
 				}
 			}
@@ -156,29 +152,28 @@ fn (mut nn NeuralNet) backprop(index int){
 			}
 			for l, mut bias_cost in hidd_lay[1]{
 				bias_cost += hidd_lay[2][l]*hidd_lay[4][l]   // It's normal that we dont apply the dsig bcz already applied 
-			}
-			
-			
+			}					
 		}else if i == nn.nb_hidden_layer-1{
-			println("antépénulflem")
 			for l, mut hidden_cost in hidd_lay[4]{
-				for j in 0..nn.nb_hidden_neurones[nn.nb_hidden_layer]{
+				for j in 0..nn.layers_list[nn.nb_hidden_layer][0].len{//Len of last layer
 					hidden_cost += nn.weights_list[nn.nb_hidden_layer][0][j][l]*nn.layers_list[nn.nb_hidden_layer][2][j]*(nn.layers_list[nn.nb_hidden_layer][3][j]-nn.excpd_outputs[index][j])  // It's normal that we dont apply the dsig bcz already applied 
 				}
 			}
-			println("typ")
 			//Weights
 			for l, mut outputlist in nn.weights_list[i][1]{
 				for j, mut weight_cost in outputlist{
-					println(hidd_lay[4][l])
 					weight_cost += nn.layers_list[i-1][3][j]*hidd_lay[2][l]*hidd_lay[4][l]  // It's normal that we dont apply the dsig bcz already applied 
 				}
 			}
-			println("k")
 			for l, mut bias_cost in hidd_lay[1]{
-				bias_cost += hidd_lay[2][l]*(hidd_lay[3][l]-nn.excpd_outputs[index][l])   // It's normal that we dont apply the dsig bcz already applied 
-			}			
+				bias_cost += hidd_lay[2][l]*hidd_lay[4][l]   // It's normal that we dont apply the dsig bcz already applied 
+			}
 		}else{
+			for l, mut hidden_cost in hidd_lay[4]{
+				for j in 0..nn.nb_hidden_neurones[i+1]{
+					hidden_cost += nn.weights_list[i+1][0][j][l]*nn.layers_list[i+1][2][j]*nn.layers_list[i+1][4][j]  // It's normal that we dont apply the dsig bcz already applied 
+				}
+			}
 			//Weights
 			for k, mut outputlist in nn.weights_list[i][1]{
 				for j, mut weight_cost in outputlist{
@@ -188,20 +183,13 @@ fn (mut nn NeuralNet) backprop(index int){
 			for l, mut bias_cost in hidd_lay[1]{
 				bias_cost += hidd_lay[2][l]*hidd_lay[4][l]   // It's normal that we dont apply the dsig bcz already applied 
 			}
-			for l, mut hidden_cost in hidd_lay[4]{
-				for j in 0..nn.nb_hidden_neurones[1]{
-					hidden_cost += nn.weights_list[i+1][0][j][l]*nn.layers_list[i+1][2][j]*nn.layers_list[i+1][4][j]  // It's normal that we dont apply the dsig bcz already applied 
-				}
-			}
 		}
-		
-		
 	}
 }
 
 fn (mut nn NeuralNet) apply_delta(){
 	//Output Weights
-	for l, mut hidd_lay in nn.weights_list{
+	for mut hidd_lay in nn.weights_list{
 		for i, mut w_list in hidd_lay[0]{
 			for j, mut weight in w_list{
 				weight -= hidd_lay[1][i][j] * nn.learning_rate
@@ -209,77 +197,106 @@ fn (mut nn NeuralNet) apply_delta(){
 		}
 	}
 
-	for h, mut hidd_lay in nn.layers_list{
+	for mut hidd_lay in nn.layers_list{
 		for i, mut bias in hidd_lay[0]{
 			bias -= hidd_lay[1][i] * nn.learning_rate
 		}
 	}
 }
 
-//not done yet
-fn main(){
-	mut neunet := NeuralNet{		
-								learning_rate: 0.3
-								inputs: [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
-								excpd_outputs: [[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [0.0, 1.0]]
-								nb_inputs: nb_i 
-								nb_hidden_layer: nb_hl
-								nb_hidden_neurones: hd_neu 
-								nb_outputs: nb_o
+fn (mut nn NeuralNet) randomise_i_exp_o(){
+	mut base_inputs := nn.inputs.clone()
+	range := base_inputs.len
+	mut base_expd_o := nn.excpd_outputs.clone()
+	nn.inputs.clear()
+	nn.excpd_outputs.clear()
+	for _ in 0..range{
+		i := rd.int_in_range(0, base_inputs.len) or {panic(err)}
+		nn.inputs << base_inputs[i]
+		base_inputs.delete(i)
+		nn.excpd_outputs << base_expd_o[i]
+		base_expd_o.delete(i)
+	}
+}
 
-								weights_list: [][][][]f64{len:nb_hl+1, init:[][][]f64{len:2, init:[][]f64{}}}
-								layers_list: [][][]f64{len:nb_hl+1, init:[][]f64{len:5, init:[]f64{}}}
-								glob_output: [][]f64{len:4, init:[]f64{len:nb_i}}
-							}
-	for i in 0..nb_hl+1{
+fn (mut nn NeuralNet) init(){
+	nn.weights_list = [][][][]f64{len:nn.nb_hidden_layer+1, init:[][][]f64{len:2, init:[][]f64{}}}
+	nn.layers_list = [][][]f64{len:nn.nb_hidden_layer+1, init:[][]f64{len:5, init:[]f64{}}}
+	nn.glob_output = [][]f64{len:4, init:[]f64{len:nn.nb_inputs}}
+	for i in 0..nn.nb_hidden_layer+1{
 		if i == 0{
-			neunet.weights_list[i][0] = [][]f64{len:hd_neu[0], init:[]f64{len:nb_i}}
-			neunet.weights_list[i][1] = [][]f64{len:hd_neu[0], init:[]f64{len:nb_i}}
+			nn.weights_list[i][0] = [][]f64{len:nn.nb_hidden_neurones[0], init:[]f64{len:nn.nb_inputs}}
+			nn.weights_list[i][1] = [][]f64{len:nn.nb_hidden_neurones[0], init:[]f64{len:nn.nb_inputs}}
 		}
-		else if i == nb_hl{
-			neunet.weights_list[i][0] = [][]f64{len:nb_o, init:[]f64{len:hd_neu[i-1]}}
-			neunet.weights_list[i][1] = [][]f64{len:nb_o, init:[]f64{len:hd_neu[i-1]}}
+		else if i == nn.nb_hidden_layer{
+			nn.weights_list[i][0] = [][]f64{len:nn.nb_outputs, init:[]f64{len:nn.nb_hidden_neurones[i-1]}}
+			nn.weights_list[i][1] = [][]f64{len:nn.nb_outputs, init:[]f64{len:nn.nb_hidden_neurones[i-1]}}
 		}else{
-			neunet.weights_list[i][0] = [][]f64{len:hd_neu[i], init:[]f64{len:hd_neu[i-1]}}
-			neunet.weights_list[i][1] = [][]f64{len:hd_neu[i], init:[]f64{len:hd_neu[i-1]}}
+			nn.weights_list[i][0] = [][]f64{len:nn.nb_hidden_neurones[i], init:[]f64{len:nn.nb_hidden_neurones[i-1]}}
+			nn.weights_list[i][1] = [][]f64{len:nn.nb_hidden_neurones[i], init:[]f64{len:nn.nb_hidden_neurones[i-1]}}
 		}
 	}
-	for i in 0..nb_hl+1{
-		if i == nb_hl{
-			neunet.layers_list[i][0] = []f64{len:nb_o}
-			neunet.layers_list[i][1] = []f64{len:nb_o}
-			neunet.layers_list[i][2] = []f64{len:nb_o}
-			neunet.layers_list[i][3] = []f64{len:nb_o}
-			neunet.layers_list[i][4] = []f64{len:nb_o}
+	for i in 0..nn.nb_hidden_layer+1{
+		if i == nn.nb_hidden_layer{
+			nn.layers_list[i][0] = []f64{len:nn.nb_outputs}
+			nn.layers_list[i][1] = []f64{len:nn.nb_outputs}
+			nn.layers_list[i][2] = []f64{len:nn.nb_outputs}
+			nn.layers_list[i][3] = []f64{len:nn.nb_outputs}
+			nn.layers_list[i][4] = []f64{len:nn.nb_outputs}
 		}else{
-			neunet.layers_list[i][0] = []f64{len:hd_neu[i]}
-			neunet.layers_list[i][1] = []f64{len:hd_neu[i]}
-			neunet.layers_list[i][2] = []f64{len:hd_neu[i]}
-			neunet.layers_list[i][3] = []f64{len:hd_neu[i]}
-			neunet.layers_list[i][4] = []f64{len:hd_neu[i]}
+			nn.layers_list[i][0] = []f64{len:nn.nb_hidden_neurones[i]}
+			nn.layers_list[i][1] = []f64{len:nn.nb_hidden_neurones[i]}
+			nn.layers_list[i][2] = []f64{len:nn.nb_hidden_neurones[i]}
+			nn.layers_list[i][3] = []f64{len:nn.nb_hidden_neurones[i]}
+			nn.layers_list[i][4] = []f64{len:nn.nb_hidden_neurones[i]}
 		}
 	}
-	neunet.set_rd_wb_values()
+	nn.set_rd_wb_values()
+}
+
+fn (mut nn NeuralNet) train(nb_epochs u64){
 	for epoch in 0..nb_epochs{
-		for mut hidd_lay in neunet.weights_list{
+		if nn.shuffle_dataset{
+			nn.randomise_i_exp_o()
+		}
+		for mut hidd_lay in nn.weights_list{
 			for mut costs_list in hidd_lay[1]{
 				costs_list = []f64{len:costs_list.len}
 			}
 		}
-		for mut hidd_lay in neunet.layers_list{
+		for mut hidd_lay in nn.layers_list{
 			hidd_lay[1] = []f64{len:hidd_lay[1].len}
 			hidd_lay[4] = []f64{len:hidd_lay[4].len}
 		}
-		for i, _ in neunet.inputs{
-			neunet.reset()
-			neunet.forward_prop(i)
-			neunet.backprop(i)
-			neunet.glob_output[i] = neunet.layers_list[nb_hl][3]
+		for i, _ in nn.inputs{
+			nn.reset()
+			nn.forward_prop(i)
+			nn.backprop(i)
+			nn.glob_output[i] = nn.layers_list[nn.nb_hidden_layer][3]
 		}
-		if epoch%10000 == 0{
-			println('\nEpoch: $epoch Cost: ${neunet.layers_list[nb_hl][4]} \nOutputs: $neunet.glob_output \nExpected Outputs: $neunet.excpd_outputs')
+		if nn.print_epoch > 0{
+			if epoch%10000 == 0{
+				println('\nEpoch: $epoch Cost: ${nn.layers_list[nn.nb_hidden_layer][4]} \nOutputs: $nn.glob_output \nExpected Outputs: $nn.excpd_outputs')
+			}
 		}
-		neunet.apply_delta()
+		nn.apply_delta()
 	}
-	println('____________________________________________________________\nFinal Results: \nCost: ${neunet.layers_list[nb_hl][4]} \nOutputs: $neunet.glob_output \nExpected Outputs: $neunet.excpd_outputs')
+	println('____________________________________________________________\nFinal Results: \nCost: ${nn.layers_list[nn.nb_hidden_layer][4]} \nOutputs: $nn.glob_output \nExpected Outputs: $nn.excpd_outputs')
+}
+
+fn main(){
+	nb_epochs := 100000
+	mut neunet := NeuralNet{		
+								learning_rate: 0.3
+								inputs: [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
+								excpd_outputs: [[0.0], [0.0], [0.0], [1.0]]
+								nb_inputs: 2 
+								nb_hidden_layer: 1
+								nb_hidden_neurones: [10] 
+								nb_outputs: 1
+								shuffle_dataset: true
+								print_epoch: 10000
+							}
+	neunet.init()
+	neunet.train(u64(nb_epochs))
 }
